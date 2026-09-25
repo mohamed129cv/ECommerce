a99629875667dc0eb703a131df66674d1a2d8186
Mohamed129cv <mohamed129cv@gmail.com>
2026-09-25 15:21:36 +0300

    Auto-generated commit

--- chunk-ZDOUZZIO.js
+++
@@ -1,1 +0,0 @@
-import{Xa as o,Ya as a,_ as n,gb as p,ob as s}from"./chunk-KFM3YCVN.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-not-found"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,m){e&1&&(o(0,"p"),p(1,"not-found works!"),a())}})};export{i as NotFoundComponent};
